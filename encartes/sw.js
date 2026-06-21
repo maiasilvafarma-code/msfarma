aabc67907fd724090c05c25cc830a3dc248e2ab1
maiasilvafarma-code <maiasilvafarma@gmail.com>
2026-06-21 15:40:05 -0300

    PWA: encartes/sw.js

---
+++ encartes/sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'msfarma-encartes-v1';
+const ASSETS = [
+  '/msfarma/encartes/',
+  '/msfarma/encartes/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
